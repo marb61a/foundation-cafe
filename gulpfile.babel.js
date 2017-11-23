f15ebbaf87639e3118784a34c95607c97d6ed4b5
martin byrne <marb61a@gmail.com>
2017-11-23 11:50:47 +0000

    updated dependencies and babelfile

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,26 +1,25 @@
 'use strict';
 
-import plugins from 'gulp-load-plugins';
-import yargs from 'yargs';
-import browser from 'browser-sync';
-import gulp from 'gulp';
-import panini from 'panini';
-import rimraf from 'rimraf';
-import sherpa from 'style-sherpa';
-import yaml from 'js-yaml';
-import fs from 'fs';
+import plugins       from 'gulp-load-plugins';
+import yargs         from 'yargs';
+import browser       from 'browser-sync';
+import gulp          from 'gulp';
+import panini        from 'panini';
+import rimraf        from 'rimraf';
+import sherpa        from 'style-sherpa';
+import yaml          from 'js-yaml';
+import fs            from 'fs';
 import webpackStream from 'webpack-stream';
-import webpack2 from 'webpack';
-import named from 'vinyl-named';
+import webpack2      from 'webpack';
+import named         from 'vinyl-named';
 
-
-// Load all of the Gulp plugins into a single variable
+// Load all Gulp plugins into one variable
 const $ = plugins();
 
-// Check for the production flag
+// Check for --production flag
 const PRODUCTION = !!(yargs.argv.production);
 
-// Load settings from the settings.yml file
+// Load settings from settings.yml
 const { COMPATIBILITY, PORT, UNCSS_OPTIONS, PATHS } = loadConfig();
 
 function loadConfig() {
@@ -28,56 +27,57 @@ function loadConfig() {
   return yaml.load(ymlFile);
 }
 
-// Build the dist folder by running the tasks listed below
-gulp.task('build', gulp.series(
-  clean, gulp.parallel(pages, sass, javascript, images, copy), styleGuide  
-));
+// Build the "dist" folder by running all of the below tasks
+gulp.task('build',
+ gulp.series(clean, gulp.parallel(pages, sass, javascript, images, copy), styleGuide));
 
-// Build the site then run the server and watch for changes
-gulp.task('default', gulp.series(
-  'build', server, watch
-));
+// Build the site, run the server, and watch for file changes
+gulp.task('default',
+  gulp.series('build', server, watch));
 
-// Delete the dist folder, this happens everytime starting a build
-function clean(done){
+// Delete the "dist" folder
+// This happens every time a build starts
+function clean(done) {
   rimraf(PATHS.dist, done);
 }
 
-// Copy files out of the asssets folder, this skips img, js and scss files which are separately parsed
-function copy(){
+// Copy files out of the assets folder
+// This task skips over the "img", "js", and "scss" folders, which are parsed separately
+function copy() {
   return gulp.src(PATHS.assets)
-    .pipe(gulp.dest(PATHS.dist, '/assets'));
+    .pipe(gulp.dest(PATHS.dist + '/assets'));
 }
 
 // Copy page templates into finished HTML files
-function pages(){
+function pages() {
   return gulp.src('src/pages/**/*.{html,hbs,handlebars}')
     .pipe(panini({
       root: 'src/pages/',
       layouts: 'src/layouts/',
-      partials: 'src/partials',
+      partials: 'src/partials/',
       data: 'src/data/',
-      helpers: 'src/helpers'
+      helpers: 'src/helpers/'
     }))
     .pipe(gulp.dest(PATHS.dist));
 }
 
-// Load updated Html templates and partials into panini
-function resetPages(done){
+// Load updated HTML templates and partials into Panini
+function resetPages(done) {
   panini.refresh();
   done();
 }
 
 // Generate a style guide from the Markdown content and HTML template in styleguide/
-function styleGuide(done){
+function styleGuide(done) {
   sherpa('src/styleguide/index.md', {
     output: PATHS.dist + '/styleguide.html',
     template: 'src/styleguide/template.html'
   }, done);
 }
 
-// Compile Sass into CSS which in production is compressed
-function sass(){
+// Compile Sass into CSS
+// In production, the CSS is compressed
+function sass() {
   return gulp.src('src/assets/scss/app.scss')
     .pipe($.sourcemaps.init())
     .pipe($.sass({
@@ -87,12 +87,12 @@ function sass(){
     .pipe($.autoprefixer({
       browsers: COMPATIBILITY
     }))
-    // Uncomment the below pipe to run UnCSS in a production environment
-    // .pipe($.if(PRODUCTION, $.uncss(UNCSS_OPTIONS)))
+    // Comment in the pipe below to run UnCSS in production
+    //.pipe($.if(PRODUCTION, $.uncss(UNCSS_OPTIONS)))
     .pipe($.if(PRODUCTION, $.cleanCss({ compatibility: 'ie9' })))
     .pipe($.if(!PRODUCTION, $.sourcemaps.write()))
     .pipe(gulp.dest(PATHS.dist + '/assets/css'))
-    .pipe(browser.reload({stream: true}));
+    .pipe(browser.reload({ stream: true }));
 }
 
 let webpackConfig = {
@@ -103,15 +103,15 @@ let webpackConfig = {
         use: [
           {
             loader: 'babel-loader'
-          }  
+          }
         ]
-      }    
+      }
     ]
   }
-};
-
-// Combine the JS into one file which is then minified in production
-function javascript(){
+}
+// Combine JavaScript into one file
+// In production, the file is minified
+function javascript() {
   return gulp.src(PATHS.entries)
     .pipe(named())
     .pipe($.sourcemaps.init())
@@ -123,8 +123,9 @@ function javascript(){
     .pipe(gulp.dest(PATHS.dist + '/assets/js'));
 }
 
-// Copy images into the dist folder which are compressed in production
-function images(){
+// Copy images to the "dist" folder
+// In production, the images are compressed
+function images() {
   return gulp.src('src/assets/img/**/*')
     .pipe($.if(PRODUCTION, $.imagemin({
       progressive: true
@@ -132,9 +133,8 @@ function images(){
     .pipe(gulp.dest(PATHS.dist + '/assets/img'));
 }
 
-
-// Start a server with BrowserSync to preview site in
-function server(done){
+// Start a server with BrowserSync to preview the site in
+function server(done) {
   browser.init({
     server: PATHS.dist, port: PORT
   });
@@ -142,13 +142,13 @@ function server(done){
 }
 
 // Reload the browser with BrowserSync
-function reload(done){
+function reload(done) {
   browser.reload();
   done();
 }
 
-// Watch for changes to Static Assets, Page, Sass, JS
-function watch(){
+// Watch for changes to static assets, pages, Sass, and JavaScript
+function watch() {
   gulp.watch(PATHS.assets, copy);
   gulp.watch('src/pages/**/*.html').on('all', gulp.series(pages, browser.reload));
   gulp.watch('src/{layouts,partials}/**/*.html').on('all', gulp.series(resetPages, pages, browser.reload));
